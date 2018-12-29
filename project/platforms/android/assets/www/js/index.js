c97028ec3db255a4f248e87ebb4426649da9f779
sidxyz <sidrt18@gmail.com>
2018-12-29 13:04:58 +0530

    adding fingerprint and contacts plugin

--- project/platforms/android/assets/www/js/index.js
+++ project/platforms/android/assets/www/js/index.js
@@ -11,13 +11,25 @@ var app = {
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
         var baseUrl = localStorage.getItem('baseUrl');
-        $.get(baseUrl+'/showAppName',function(data)
-        {
-            var heading = document.querySelector('#appHeading');
-            heading.innerHTML=data;
-            console.log(data);
-           
-        });
+        console.log(navigator.contacts);
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var filter = ["displayName", "addresses"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+
+        var myContact = navigator.contacts.create({"displayName": "MeFreelancer"});
+        var phoneNumbersArray = [];
+        
+        phoneNumbersArray[0] = new ContactField('work', '768-555-1234', false);
+        phoneNumbersArray[1] = new ContactField('mobile', '999-555-5432', true); // preferred number
+        phoneNumbersArray[2] = new ContactField('home', '203-555-7890', false);
+        myContact.phoneNumbers = phoneNumbersArray;
+       // myContact.save(contactSuccessCallBack,contactErrorCallBack);
+
+       Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
+ 
+       
 
     },
 
@@ -27,6 +39,64 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function onSuccess(contacts) 
+{
+    console.log(contacts);
+    // for (var i = 0; i < contacts.length; i++) {
+    //     for (var j = 0; j < contacts[i].addresses.length; j++) {
+    //         console.log(contacts[i]);
+    //         // alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
+    //         //     "Type: "           + contacts[i].addresses[j].type          + "\n" +
+    //         //     "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+    //         //     "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+    //         //     "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+    //         //     "Region: "         + contacts[i].addresses[j].region        + "\n" +
+    //         //     "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+    //         //     "Country: "        + contacts[i].addresses[j].country);
+    //     }
+    // }
+};
+ 
+function onError(contactError) {
+    alert('onError!');
+};
+
+function contactSuccessCallBack(){
+    console.log("contact successfully created");
+}
+ function contactErrorCallBack()
+ {  
+    alert('Error while saving contact!');
+ }
+
+ function isAvailableSuccess(result) {
+    /*
+    result depends on device and os. 
+    iPhone X will return 'face' other Android or iOS devices will return 'finger'  
+    */
+    console.log("Fingerprint available");
+    Fingerprint.show({
+        clientId: "Fingerprint-Demo",
+        clientSecret: "password", //Only necessary for Android
+        disableBackup:"true"
+      },fingerPrintSuccessCallback, fingerPrintErrorCallback);
+  }
+
+  function fingerPrintSuccessCallback()
+  {
+    alert("fingerprint auth successfull!");
+  }
+
+  function fingerPrintErrorCallback(err)
+  {
+    alert("Authentication invalid " + err);
+  }
+
+  function isAvailableError(message) {
+    alert("finger print is not available, message = "+message);
+  } 
+
 
 app.initialize();
 
--- project/www/js/index.js
+++ project/www/js/index.js
@@ -11,14 +11,32 @@ var app = {
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
         var baseUrl = localStorage.getItem('baseUrl');
-        $.get(baseUrl+'/showAppName',function(data)
-        {
-            var heading = document.querySelector('#appHeading');
-            heading.innerHTML=data;
-            console.log(data);
-           
-        });
 
+        //to check if contact plugin works
+        console.log(navigator.contacts);
+
+        //code to fetch all contacts
+        var options = new ContactFindOptions();
+        options.filter = "";
+        options.multiple = true;
+        var filter = ["displayName", "addresses"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
+
+        //code to create new contact
+        var myContact = navigator.contacts.create({"displayName": "MeFreelancer"});
+        var phoneNumbersArray = [];
+
+        //adding multiple phone numbers to the contact
+        phoneNumbersArray[0] = new ContactField('work', '768-555-1234', false);
+        phoneNumbersArray[1] = new ContactField('mobile', '999-555-5432', true);
+        phoneNumbersArray[2] = new ContactField('home', '203-555-7890', false);
+        myContact.phoneNumbers = phoneNumbersArray;
+
+        //saving to the phone
+        myContact.save(contactSuccessCallBack,contactErrorCallBack);
+
+        //to check if phone supports fingerprint or not
+        Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
     },
 
     // Update DOM on a Received Event
@@ -27,6 +45,63 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function onSuccess(contacts) 
+{
+    console.log(contacts);
+    // for (var i = 0; i < contacts.length; i++) {
+    //     for (var j = 0; j < contacts[i].addresses.length; j++) {
+    //         console.log(contacts[i]);
+    //         // alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
+    //         //     "Type: "           + contacts[i].addresses[j].type          + "\n" +
+    //         //     "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+    //         //     "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+    //         //     "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+    //         //     "Region: "         + contacts[i].addresses[j].region        + "\n" +
+    //         //     "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+    //         //     "Country: "        + contacts[i].addresses[j].country);
+    //     }
+    // }
+};
+ 
+function onError(contactError) {
+    alert('Error While fetching all contacts! Error message ='+contactError);
+};
+
+function contactSuccessCallBack(){
+    console.log("contact successfully created");
+}
+ function contactErrorCallBack()
+ {  
+    alert('Error while saving contact!');
+ }
+
+ function isAvailableSuccess(result) {
+    /*
+    result depends on device and os. 
+    iPhone X will return 'face' other Android or iOS devices will return 'finger'  
+    */
+    console.log("Fingerprint available");
+    Fingerprint.show({
+        clientId: "Fingerprint-Demo",
+        clientSecret: "password", //Only necessary for Android
+        disableBackup:"true" //To disable backup method like pin or password if you dont want fingerprint auth
+      },fingerPrintSuccessCallback, fingerPrintErrorCallback);
+  }
+
+  function fingerPrintSuccessCallback()
+  {
+    alert("fingerprint auth successfull!");
+  }
+
+  function fingerPrintErrorCallback(err)
+  {
+    alert("Authentication invalid " + err);
+  }
+
+  function isAvailableError(message) {
+    alert("finger print is not available, message = "+message);
+  } 
 
 app.initialize();
 
